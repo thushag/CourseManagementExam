bb62128ef10e76f5f67a9ff23d7883f3091ccb72
Thushanthini G. <UT01138@unicomtic.com>
2024-10-09 14:56:55 +0530

    question1

---
+++ CourseManagement/Course.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CourseManagement
+{
+    public class Course
+    {
+        public int CourseID { get; set; }  
+        public string Title { get; set; }
+        public string Duration { get; set; }
+        public decimal Price { get; set; }
+
+    }
+}
